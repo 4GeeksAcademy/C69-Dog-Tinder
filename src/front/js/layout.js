840ae331b286dba7e1d0caa94032d84ac7281efb
KOTHECODE <160642762+KOTHECODE@users.noreply.github.com>
2024-09-28 00:16:19 +0000

    fix log in page

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -4,12 +4,11 @@ import ScrollToTop from "./component/scrollToTop";
 import { BackendURL } from "./component/backendURL";
 
 import { Home } from "./pages/home";
-import { Demo } from "./pages/demo";
-import { Single } from "./pages/single";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
+import { LoginSignUp } from "./LoginSignUp";
 
 //create your first component
 const Layout = () => {
@@ -26,8 +25,7 @@ const Layout = () => {
                     <Navbar />
                     <Routes>
                         <Route element={<Home />} path="/" />
-                        <Route element={<Demo />} path="/demo" />
-                        <Route element={<Single />} path="/single/:theid" />
+                        <Route element={<LoginSignUp />} path="/" />
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
                     <Footer />
