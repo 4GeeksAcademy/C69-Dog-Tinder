cf65c68f86e4099d7f2fd33360915476d43dc926
Brenda Marquez <brenda.jmarq@gmail.com>
2024-10-08 02:35:26 +0000

    Add settings page with background image and age conversion

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,58 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FaDog, FaHeart, FaComment, FaCog } from 'react-icons/fa'; // Example icons
+import '../../styles/navbar.css';
 
 const Navbar = () => {
   return (
     <>
-      {/* Navbar Toggler for Sidebar */}
-      <nav className="navbar navbar-light bg-light fixed-top">
-        <div className="container-fluid">
-          <button
-            className="btn btn-primary"
-            type="button"
-            data-bs-toggle="offcanvas"
-            data-bs-target="#offcanvasNavbar"
-            aria-controls="offcanvasNavbar"
-          >
-            ☰
-          </button>
-          <Link className="navbar-brand ms-3" to="/">Dog Tinder</Link>
+      {/* Bottom Navbar */}
+      <nav className="navbar navbar-light bg-light fixed-bottom app-navbar">
+        <div className="container-fluid justify-content-around">
+          <Link className="nav-item text-center" to="/profiles">
+            <FaDog className="navbar-icon" />
+            <span className="navbar-text">Profiles</span>
+          </Link>
+
+          <Link className="nav-item text-center" to="/playdates">
+            <FaHeart className="navbar-icon" />
+            <span className="navbar-text">Playdates</span>
+          </Link>
+
+          <Link className="nav-item text-center" to="/chats">
+            <FaComment className="navbar-icon" />
+            <span className="navbar-text">Chats</span>
+          </Link>
+
+          <Link className="nav-item text-center" to="/settings">
+            <FaCog className="navbar-icon" />
+            <span className="navbar-text">Settings</span>
+          </Link>
         </div>
       </nav>
-
-      {/* Off-Canvas Sidebar */}
-      <div
-        className="offcanvas offcanvas-start"
-        tabIndex="-1"
-        id="offcanvasNavbar"
-        aria-labelledby="offcanvasNavbarLabel"
-      >
-        <div className="offcanvas-header">
-          <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
-          <button
-            type="button"
-            className="btn-close"
-            data-bs-dismiss="offcanvas"
-            aria-label="Close"
-          ></button>
-        </div>
-        <div className="offcanvas-body">
-          <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-            <li className="nav-item">
-              <Link className="nav-link" to="/profiles">Profiles</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/playdates">Playdates</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/chat">Chats</Link>  {/* Corrected path */}
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/settings">Settings</Link>
-            </li>
-          </ul>
-        </div>
-      </div>
     </>
   );
 };
